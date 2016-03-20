e8b55003a404235717ce96d92784667ecbf5ddf6
烟树 <givemearainbowlife@gmail.com>
2016-03-20 22:08:48 +0800

    add fetDate demo for test

--- Application/Common/Conf/config.tmp.php
+++ Application/Common/Conf/config.tmp.php
@@ -6,10 +6,10 @@ return array(
 
   /* 数据库配置 */
   'DB_TYPE'   => 'mysql', // 数据库类型
-  'DB_HOST'   => 'localhost', // 服务器地址
-  'DB_NAME'   => 'hw', // 数据库名
-  'DB_USER'   => 'root ', // 用户名
-  'DB_PWD'    => 'ahbing',  // 密码
+  'DB_HOST'   => '', // 服务器地址
+  'DB_NAME'   => '', // 数据库名
+  'DB_USER'   => ' ', // 用户名
+  'DB_PWD'    => '',  // 密码
   'DB_PORT'   => '3306', // 端口
   'DB_PREFIX' => 'hw_', // 数据库表前缀
 
